363d56eaa627bf2f7539f3a2ee5fb1260e1de2fb
Mohammed Aeraf <aeraf@outlook.com>
2026-05-10 21:15:21 +0530

    First Commit

---
+++ my-app/src/App.tsx
@@ -0,0 +1,12 @@
+import Courses from "./Courses";
+
+function App() {
+  return (
+    <div>
+      <h1>My Courses App</h1>
+      <Courses></Courses>
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/Courses.tsx
@@ -0,0 +1,16 @@
+// TSX - TypeScript XML
+function Courses() {
+  return (
+    <div>
+      <h2>Course List</h2>
+      <ul>
+        <li>Full Stack MERN</li>
+        <li>Web Design</li>
+        <li>Python</li>
+        <li>Data Analytics</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Courses;
