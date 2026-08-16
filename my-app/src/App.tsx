ff403d7d22cfc91279fba94b89cfefcf84f9ceff
mohammedaeraf <aeraf@outlook.com>
2026-08-16 20:15:14 +0530

    Recipe Add Component

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,10 +1,13 @@
-import RecipeList from "./RecipeList";
+import RecipeAdd from "./RecipeAdd";
+// import RecipeList from "./RecipeList";
 
 function App() {
   return (
     <div className="container">
-      <h1 className="text-center text-primary">Recipe App</h1>
-      <RecipeList></RecipeList>
+      <h1 className="text-center text-danger py-5 fw-bold display-5">
+        Recipe App
+      </h1>
+      <RecipeAdd></RecipeAdd>
     </div>
   );
 }
